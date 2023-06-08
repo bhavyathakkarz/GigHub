3804825d656e3353a17c6ddd993a1413c000252d
Bhavya <thakkarbhavya4@gmail.com>
2023-06-08 21:34:22 +0530

    initial

---
+++ client/src/App.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import Gig from "./pages/gig/Gig";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Add from "./pages/add/Add";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import Gigs from "./pages/gigs/Gigs";
+import Register from "./pages/register/Register";
+import Orders from "./pages/orders/Orders";
+import "./app.scss";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.scss";
+import Featured from "../../components/featured/Featured";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Featured />
+    </div>
+  );
+};
+
+export default Home;
