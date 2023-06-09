ad0ae790ca3f687c30b536153b78e15736d06f77
Bhavya <thakkarbhavya4@gmail.com>
2023-06-09 17:47:30 +0530

    changes

---
+++ client/src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./trustedBy.scss";
+
+const TrustedBy = () => {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <span>Trusted By:</span>
+        <img src="./img/meta.png" alt="tech" />
+        <img src="./img/google.png" alt="tech" />
+        <img src="./img/netflix.png" alt="tech" />
+        <img src="./img/pandg.png" alt="tech" />
+        <img src="./img/paypal.png" alt="tech" />
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
