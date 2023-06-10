bf2f0c6a017e77f65012133732ac7e181f086533
Bhavya <thakkarbhavya4@gmail.com>
2023-06-10 19:20:12 +0530

    chabgess

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,8 +3,9 @@ import "./home.scss";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slide/Slide";
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 const Home = () => {
   return (
@@ -58,6 +59,47 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>
+              fiverr <span>business.</span>
+            </h1>
+            <h1>
+              A solution built for <i>business</i>
+            </h1>
+            <p>
+              Upgrade to a curated experience to access vetted talent and
+              exclusive tools
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Talent matching
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Dedicated account management
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Team collaboration tools
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Business payment solutions
+            </div>
+            <button>Explore Fiverr Business</button>
+          </div>
+          <div className="item">
+            <img src="./img/businesses.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} item={project} />
+        ))}
+      </Slide>
     </div>
   );
 };
